0b948b6530f3a86aa2273fa1da0eabf24a50b276
Javier Moneo <jvr.moneo@gmail.com>
2025-04-03 16:55:02 +0200

    Implement Character

--- java-ironbattle/src/main/java/org/example/Character.java
+++ java-ironbattle/src/main/java/org/example/Character.java
@@ -1,4 +1,60 @@
 package org.example;
 
-public class Character {
+import java.util.Random;
+
+public abstract class Character {
+    private String id = getSaltString();
+    private String name;
+    private int hp;
+    private boolean isAlive = true;
+
+    public Character(String name, int hp) {
+        setName(name);
+        setHp(hp);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    protected String getSaltString() {
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 18) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
+
+    }
 }
